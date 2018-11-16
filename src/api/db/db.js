f6a7b25101eb1a74556a78536dcc18e43994ffed
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-11-16 16:51:50 -0600

    add background data

--- src/api/db/db.js
+++ src/api/db/db.js
@@ -4,6 +4,7 @@ const server = jsonServer.create()
 
 const router = jsonServer.router(path.join(__dirname, 'db.json'))
 const races = require(path.join(__dirname, 'races.json'))
+const backgrounds = require(path.join(__dirname, 'backgrounds.json'))
 const classes = require(path.join(__dirname, 'classes.json'))
 
 server.use(jsonServer.defaults())
@@ -13,12 +14,14 @@ server.use(
   jsonServer.rewriter({
     '/races/:id': '/races?id=:id',
     '/classes/:id': '/classes?id=:id',
+    '/backgrounds/:id': '/backgrounds?id=:id',
   })
 )
 
 // routers
 server.get('/races', jsonServer.router(races))
 server.get('/classes', jsonServer.router(classes))
+server.get('/classes', jsonServer.router(backgrounds))
 server.use(router)
 
 server.listen(3000, () => {
