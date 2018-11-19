76965f8653e64ef239be756a95e9c5c615b9c1e3
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-11-19 15:17:07 -0600

    hptile styling

--- src/components/elements/HpTile.vue
+++ src/components/elements/HpTile.vue
@@ -1,19 +1,27 @@
 <template>
-  <div class="hp">
-    <stat-tile
-      :label="'Hit Points'"
-      :value="value"
-      :suffix="'/'+max.toString()"
-      :adjustable="true"
-    ></stat-tile>
-    <div class="meter"></div>
+  <div class="statTile hpTile">
+    <div>
+      <label>Hit Points</label>
+      <adjustable-number
+        :value="value"
+        :suffix="'/'+max.toString()"
+        @increment="$emit('increment')"
+        @decrement="$emit('decrement')"
+      >
+      </adjustable-number>
+      <div class="meter">
+        <div :class="{health: true, red: value/max < .25}" :style="{width: (value/max)*100+'%'}"></div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
-import StatTile from './StatTile.vue'
+import AdjustableNumber from './AdjustableNumber.vue'
+import '../../assets/scss/components/statTile.scss'
+import '../../assets/scss/components/hpTile.scss'
 export default {
   components: {
-    StatTile,
+    AdjustableNumber,
   },
   props: {
     value: Number,
