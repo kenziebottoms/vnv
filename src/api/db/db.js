b45950367c611b902b7e245d98b1b2c370e2dc7b
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-11-20 10:13:47 -0600

    monster db and route

--- src/api/db/db.js
+++ src/api/db/db.js
@@ -5,6 +5,7 @@ const server = jsonServer.create()
 const router = jsonServer.router(path.join(__dirname, 'db.json'))
 const races = require(path.join(__dirname, 'races.json'))
 const backgrounds = require(path.join(__dirname, 'backgrounds.json'))
+const monsters = require(path.join(__dirname, 'monsters.json'))
 const classes = require(path.join(__dirname, 'classes.json'))
 
 server.use(jsonServer.defaults())
@@ -15,13 +16,15 @@ server.use(
     '/races/:id': '/races?id=:id',
     '/classes/:id': '/classes?id=:id',
     '/backgrounds/:id': '/backgrounds?id=:id',
+    '/monsters/:id': '/monsters?id=:id',
   })
 )
 
 // routers
 server.get('/races', jsonServer.router(races))
 server.get('/classes', jsonServer.router(classes))
-server.get('/classes', jsonServer.router(backgrounds))
+server.get('/backgrounds', jsonServer.router(backgrounds))
+server.get('/monsters', jsonServer.router(monsters))
 server.use(router)
 
 server.listen(3000, () => {
