c2865c6ced1f274009219ea666fedf62a10476ad
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-11-20 18:20:02 -0600

    Rearrange styling, make headings actually big

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import scss from './assets/scss/style.scss'
 import bulmaCss from 'bulma/css/bulma.min.css'
+import scss from './assets/scss/style.scss'
 
 Vue.config.productionTip = false
 
