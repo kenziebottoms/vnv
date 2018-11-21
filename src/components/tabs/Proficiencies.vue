d72911a070c85f62705efe67200d5f456ccbb9c0
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-11-20 18:18:01 -0600

    list class and race proficiencies

--- src/components/tabs/Proficiencies.vue
+++ src/components/tabs/Proficiencies.vue
@@ -4,7 +4,11 @@
     <section v-if="char.class.proficiencies.skills">
       <h3>By Class</h3>
       <ul>
-        <li :key="index" v-for="(options, index) in char.class.proficiencies.skills">
+        <li
+          :key="index"
+          v-for="(options, index) in char.class.proficiencies.skills"
+          v-if="char.class.proficiencies.skills.length > 1"
+        >
           <dropdown
             :id="'classSkillProficiency-'+index"
             :label="'Choose a skill proficiency'"
@@ -12,6 +16,25 @@
             :items="options"
           ></dropdown>
         </li>
+        <li v-else>{{options}}</li>
+      </ul>
+    </section>
+    <section v-if="char.race.proficiencies.skills">
+      <h3>By Race</h3>
+      <ul>
+        <li
+          :key="index"
+          v-for="(options, index) in char.race.proficiencies.skills"
+          v-if="char.race.proficiencies.skills.length > 1"
+        >
+          <dropdown
+            :id="'classSkillProficiency-'+index"
+            :label="'Choose a skill proficiency'"
+            v-model="char.skills.raceProficiencies[index]"
+            :items="options"
+          ></dropdown>
+        </li>
+        <li v-else-if="char.race.proficiencies.skills">{{char.race.proficiencies.skills[0]}}</li>
       </ul>
     </section>
     <button v-on:click="save">Save</button>
