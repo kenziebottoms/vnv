345f683406c0ab1939e9f9433f68974c11f1b5a4
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-11-26 18:17:20 -0600

    alignment chart

---
+++ src/components/elements/AlignmentGrid.vue
@@ -0,0 +1,46 @@
+<template>
+  <section id="alignment" class="grid">
+    <div></div>
+    <div class="header">Lawful</div>
+    <div class="header">Neutral</div>
+    <div class="header">Chaotic</div>
+
+    <div class="header side">Good</div>
+    <div :class="{selected: this.value == 0}">{{alignments[0]}}</div>
+    <div :class="{selected: this.value == 1}">{{alignments[1]}}</div>
+    <div :class="{selected: this.value == 2}">{{alignments[2]}}</div>
+
+    <div class="header side">Neutral</div>
+    <div :class="{selected: this.value == 3}">{{alignments[3]}}</div>
+    <div :class="{selected: this.value == 4}">{{alignments[4]}}</div>
+    <div :class="{selected: this.value == 5}">{{alignments[5]}}</div>
+
+    <div class="header side">Evil</div>
+    <div :class="{selected: this.value == 6}">{{alignments[6]}}</div>
+    <div :class="{selected: this.value == 7}">{{alignments[7]}}</div>
+    <div :class="{selected: this.value == 8}">{{alignments[8]}}</div>
+  </section>
+</template>
+<script>
+import '../../assets/scss/components/alignmentGrid.scss'
+export default {
+  data() {
+    return {
+      alignments: [
+        'Crusader',
+        'Benefactor',
+        'Rebel',
+        'Judge',
+        'Undecided',
+        'Free Spirit',
+        'Dominator',
+        'Malefactor',
+        'Destroyer',
+      ],
+    }
+  },
+  props: {
+    value: Number,
+  },
+}
+</script>
--- src/components/tabs/Personal.vue
+++ src/components/tabs/Personal.vue
@@ -1,38 +1,24 @@
 <template>
   <section v-if="char" id="personal" class="tab">
     <section>
-      <stat-tile
-        :label="'Name'"
-        :suffix="char.fullName || char.name"
-      ></stat-tile>
-      <stat-tile
-        v-if="char.race"
-        :label="'Race'"
-        :suffix="subrace.name || char.race.name"
-      ></stat-tile>
-      <stat-tile
-        v-if="char.class"
-        :label="'Class'"
-        :suffix="char.class.name"
-      ></stat-tile>
-      <stat-tile
-        v-if="char.age"
-        :label="'Age'"
-        :value="char.age"
-      ></stat-tile>
-      <stat-tile
-        v-if="char.alignment"
-        :label="'Alignment'"
-        :suffix="char.alignment"
-      ></stat-tile>
+      <stat-tile :label="'Name'" :suffix="char.fullName || char.name"></stat-tile>
+      <stat-tile v-if="char.race" :label="'Race'" :suffix="subrace.name || char.race.name"></stat-tile>
+      <stat-tile v-if="char.class" :label="'Class'" :suffix="char.class.name"></stat-tile>
+      <stat-tile v-if="char.age" :label="'Age'" :value="char.age"></stat-tile>
+    </section>
+    <h4 v-if="char.alignment">Alignment</h4>
+    <section>
+      <alignment-grid v-if="char.alignment" v-model="char.alignment"></alignment-grid>
     </section>
   </section>
 </template>
 <script>
 import StatTile from '../elements/StatTile.vue'
+import AlignmentGrid from '../elements/AlignmentGrid.vue'
 export default {
   components: {
     StatTile,
+    AlignmentGrid,
   },
   computed: {
     subrace() {
